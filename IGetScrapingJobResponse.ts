30beb90a2247f680bfcc83ccb943f420c26560d6
markussWs <74717213+markussWs@users.noreply.github.com>
2020-12-03 18:30:50 +0200

    Add files via upload

---
+++ IGetScrapingJobResponse.ts
@@ -0,0 +1,18 @@
+export interface IGetScrapingJobResponse {
+	id: number;
+	custom_id: string;
+	sitemap_name: string;
+	status: string;
+	sitemap_id: number;
+	test_run: number;
+	jobs_scheduled: number;
+	jobs_executed: number;
+	jobs_failed: number;
+	jobs_empty: number;
+	stored_record_count: number;
+	request_interval: number;
+	page_load_delay: number;
+	driver: string;
+	scheduled: number;
+	time_created: string;
+}
