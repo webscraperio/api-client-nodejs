bb5e65d677734b2b85e8765addc017bb7ae5b877
markussWs <markuss.bemers@webscraper.io>
2020-12-07 15:40:48 +0200

    add beforeEach to initialize sitemap and new Client object is now made within each test

--- test/apiClientSpec.ts
+++ test/apiClientSpec.ts
@@ -6,12 +6,9 @@ import {IGetScrapingJobResponse} from "../src/interfaces/IGetScrapingJobResponse
 import {IGetJsonResponse} from "../src/interfaces/IGetJsonResponse";
 import {IDeleteSitemap} from "../src/interfaces/IDeleteSitemap";
 
-let time: number = Date.now();
-let mySitemap = `{"_id":"${time}","startUrl":["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"],"selectors":[{"id":"element","type":"SelectorElement","parentSelectors":["_root"],"selector":"div.col-sm-4","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["element"],"selector":"a","multiple":false,"regex":"","delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["element"],"selector":"h4.pull-right","multiple":false,"regex":"","delay":0}]}`;
 const token = "kb3GZMBfRovH69RIDiHWB4GiDeg3bRgEdhDMYLJ9bcGY9PoMXl9Xf5ip4ro8";
 
-const scrapingTest = new Client(token);
-
+let mySitemap: string;
 let sitemapInfoData: ICreateSiteMapResponse;
 let scrapingJobInfo: ICreateScrapingJobResponse;
 let doingScraping: IGetScrapingJobResponse;
@@ -20,15 +17,25 @@ let deleteSitemap: IDeleteSitemap;
 
 describe("API Client", () => {
 
+	beforeEach(() => {
+		const time = Date.now();
+		mySitemap = `{"_id":"${time}","startUrl":["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"],"selectors":[{"id":"element","type":"SelectorElement","parentSelectors":["_root"],"selector":"div.col-sm-4","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["element"],"selector":"a","multiple":false,"regex":"","delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["element"],"selector":"h4.pull-right","multiple":false,"regex":"","delay":0}]}`;
+	});
+
+	afterEach(async () => {
+		const scrapingTest = new Client(token);
+		deleteSitemap = await scrapingTest.deleteSitemap(sitemapInfoData.id);
+		expect(deleteSitemap.success).to.be.equal(true);
+	});
+
 	it("should import a sitemap", async () => {
+		const scrapingTest = new Client(token);
 		sitemapInfoData = await scrapingTest.createSitemap(mySitemap);
 		expect(sitemapInfoData.id).to.not.be.undefined;
-
 	});
 
 	it("should create a scraping job", async () => {
-		time = Date.now();
-		mySitemap = `{"_id":"${time}","startUrl":["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"],"selectors":[{"id":"element","type":"SelectorElement","parentSelectors":["_root"],"selector":"div.col-sm-4","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["element"],"selector":"a","multiple":false,"regex":"","delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["element"],"selector":"h4.pull-right","multiple":false,"regex":"","delay":0}]}`;
+		const scrapingTest = new Client(token);
 
 		sitemapInfoData = await scrapingTest.createSitemap(mySitemap);
 		scrapingJobInfo = await scrapingTest.createScrapingJob(sitemapInfoData.id);
@@ -36,8 +43,7 @@ describe("API Client", () => {
 	});
 
 	it("should wait for the scraping job to end", async () => {
-		time = Date.now();
-		mySitemap = `{"_id":"${time}","startUrl":["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"],"selectors":[{"id":"element","type":"SelectorElement","parentSelectors":["_root"],"selector":"div.col-sm-4","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["element"],"selector":"a","multiple":false,"regex":"","delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["element"],"selector":"h4.pull-right","multiple":false,"regex":"","delay":0}]}`;
+		const scrapingTest = new Client(token);
 
 		sitemapInfoData = await scrapingTest.createSitemap(mySitemap);
 		scrapingJobInfo = await scrapingTest.createScrapingJob(sitemapInfoData.id);
@@ -54,8 +60,7 @@ describe("API Client", () => {
 	});
 
 	it("should get scraped json", async () => {
-		time = Date.now();
-		mySitemap = `{"_id":"${time}","startUrl":["https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"],"selectors":[{"id":"element","type":"SelectorElement","parentSelectors":["_root"],"selector":"div.col-sm-4","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["element"],"selector":"a","multiple":false,"regex":"","delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["element"],"selector":"h4.pull-right","multiple":false,"regex":"","delay":0}]}`;
+		const scrapingTest = new Client(token);
 
 		sitemapInfoData = await scrapingTest.createSitemap(mySitemap);
 		scrapingJobInfo = await scrapingTest.createScrapingJob(sitemapInfoData.id);
@@ -67,12 +72,6 @@ describe("API Client", () => {
 		}
 		scrapedJson = await scrapingTest.getJson(scrapingJobInfo.id);
 		expect(scrapedJson).to.not.be.undefined;
-	});
-
-	afterEach(async () => {
-		// runs after each test in this block
-		deleteSitemap = await scrapingTest.deleteSitemap(sitemapInfoData.id);
-		expect(deleteSitemap.success).to.be.equal(true);
 	});
 
 });
