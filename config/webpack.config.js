73516b0b2cd0ff4f5891f41e2ddda077f61fdf61
Armands Grundmanis <grundmanweb@gmail.com>
2022-04-27 15:10:45 +0300

    WS-3677 added extended config version

---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, '../dist/umd'),
+    filename: 'index.js',
+    library: 'exampleTypescriptPackage',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x*)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'config/tsconfig.umd.json',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+		"https": require.resolve("https-browserify"),
+        "url": require.resolve("url/"),
+      "http": require.resolve("stream-http"),
+      "buffer": require.resolve("buffer/"),
+      "fs": false
+    }
+  },
+}
